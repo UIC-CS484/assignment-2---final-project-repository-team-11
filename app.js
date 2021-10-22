bedb85d0a44bb1ea2c2dbd9bde30eda3ad03eca1
Sanchita Kapoor <skapoo22@uic.edu>
2021-10-22 15:07:24 -0500

    implemented passport.js

--- app.js
+++ app.js
@@ -12,7 +12,25 @@ var submit = require('./routes/submit');
 var submitLogin = require('./routes/loginSubmit');
 var dashboard = require('./routes/dashboard.js');
 
+var session = require('express-session');
+const passport = require('passport');
 var app = express();
+
+//passport config
+require('./config/passport')(passport);
+
+var session_config = {
+		secret: 'secret', //a random unique string key used to authenticate a session
+		resave: true, //nables the session to be stored back to the session store, even if the session was never modified during the request
+		saveUninitialized: true, //his allows any uninitialized session to be sent to the store. When a session is created but not modified, it is referred to as uninitialized.
+		cookie: { secure: true } //true is a recommended option. However, it requires an https-enabled website
+		//store  parameter when saving session to database
+};
+
+session_config.cookie.secure = false;
+
+//Express Sessions
+app.use(session(session_config))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +41,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
