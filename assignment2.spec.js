0e757d3492748c697f50afa02a1a886f7e548272
Sanchita Kapoor <skapoo22@uic.edu>
2021-10-22 18:38:28 -0500

    testing file

---
+++ assignment2.spec.js
@@ -0,0 +1,10 @@
+const { ExpectationFailed } = require('http-errors');
+const { TestWatcher } = require('jest');
+var passwordValidator = require('password-validator');
+
+//Testing the strength of a password
+
+test("Testing createAccount url ", ()=>
+{
+    
+})
