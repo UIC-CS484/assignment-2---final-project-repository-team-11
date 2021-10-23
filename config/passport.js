7aca6a3734d5ce1414030ebf4be435f96ec657a1
Sanchita Kapoor <skapoo22@uic.edu>
2021-10-22 21:43:46 -0500

    pushing up to master

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,23 @@
 const LocalStrategy = require('passport-local').Strategy;
+<<<<<<< HEAD
 let databaseOperations = require('../users.json');
+=======
+let users = require('../users.json');
+>>>>>>> 3c14d10b78e802751a643ca04d35ce4bd0db43f2
 
 
 module.exports = function(passport) {
 console.log("Passport Function triggered");
 //Passport pulls the the name variables from the name attribute in login form.  If different, you need to use whats on lines 10 and 11
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3c14d10b78e802751a643ca04d35ce4bd0db43f2
 passport.use(new LocalStrategy({
 	usernameField: 'email',
 	passwordField: 'password'
 }, function(username, password, done) {
+<<<<<<< HEAD
 
     var users = databaseOperations
     
@@ -17,6 +25,19 @@ passport.use(new LocalStrategy({
 
         var user = users[index];
         console.log(user.email);
+=======
+	console.log(username);
+	//Search users.json file to see if user exist
+
+
+    //let users = JSON.parse(dataStore);
+    console.log(users);
+
+    for (var index = 0; index < users.length; ++index) {
+
+        var user = users[index];
+       console.log(user.email);
+>>>>>>> 3c14d10b78e802751a643ca04d35ce4bd0db43f2
         if(user.email == username && user.password == password){
 			
           done(null, user);
@@ -25,7 +46,11 @@ passport.use(new LocalStrategy({
           done(null, false);
         }
     }
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 3c14d10b78e802751a643ca04d35ce4bd0db43f2
 }));
 
 passport.serializeUser(function(user, done) {
