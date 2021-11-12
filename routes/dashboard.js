5cb4e20f70f9b334a60b5fd0e513b0682c736d13
Gurleen <gurleenk4.23@gmail.com>
2021-11-12 14:47:45 -0600

    requirements 3 and 4 done

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     let userInfo = req.user;
+    console.log("on dashboard.js");
     console.log(req.user);
-
+    
     res.render('dashboard', {userInfo});
     
 });
